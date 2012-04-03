b9fad2359e742beba56f8d8a9f53e8994f0be2ed
Rumel <daltondick2010@gmail.com>
2012-04-03 09:58:10 -0500

    Added more comments

--- AnalyzeData.py
+++ AnalyzeData.py
@@ -4,6 +4,7 @@ import sys
 #array holding the categories that beat the other team
 winningNumbers = []
 
+#Function to compare two teams
 def findStats(team1, team2):
 	for i in range(0, 22):
 		if(i == 0 or i == 2):
@@ -15,9 +16,13 @@ def findStats(team1, team2):
 				None
 	return
 
+# The tournament file is the first argument
+# Should be in format of "1 2 1" indicating team 1 played team 2 and team 1 won
+# The team file is a csv file. I used data from kenpom.com
 tournamentFile = sys.argv[1]
 teamFile = sys.argv[2]
 
+#This goes through the teams file and adds each teams stats to the array
 teamF = open(teamFile, 'r')
 teams = []
 lineCount = 0
@@ -28,6 +33,7 @@ for line in teamF:
 	lineCount = lineCount + 1
 teamF.close()
 
+#Opens the tournament file and cycles through the games
 tournF = open(tournamentFile, 'r')
 for line in tournF:
 	game = line.split(' ')
@@ -35,6 +41,7 @@ for line in tournF:
 		findStats(teams[int(game[0])], teams[int(game[1])])
 	else:
 		findStats(teams[int(game[1])], teams[int(game[0])])
-	
-for i in range(0, 24):
+
+#counts the instance of a number to help find a correlation between winning in the tournament and stats		
+for i in range(0, 22):
 	print i, "," , winningNumbers.count(i)
